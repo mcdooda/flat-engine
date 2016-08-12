829fb444955a82b780f09e5ae2b648d298122a72
Nicolas Dodelier <nicolas.dod@gmail.com>
2016-08-12 13:24:46 +0200

    Fixed Color::RED

--- src/video/color.cpp
+++ src/video/color.cpp
@@ -7,7 +7,7 @@ namespace video
 
 Color Color::WHITE;
 Color Color::BLACK = Color(0.0f);
-Color Color::RED   = Color(1.0f, 1.0f, 1.0f, 1.0f);
+Color Color::RED   = Color(1.0f, 0.0f, 0.0f, 1.0f);
 Color Color::GREEN = Color(0.0f, 1.0f, 0.0f, 1.0f);
 Color Color::BLUE  = Color(0.0f, 0.0f, 1.0f, 1.0f);
 
