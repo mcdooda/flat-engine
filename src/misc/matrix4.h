c2bdc4f2a64a6d170e1155da36db6cd9137a534f
mcdooda <nicolas.dod@gmail.com>
2016-09-08 00:02:38 +0200

    Added overriden << operator for Vector2/3/4 and Matrix4

--- src/misc/matrix4.h
+++ src/misc/matrix4.h
@@ -1,6 +1,8 @@
 #ifndef FLAT_GEOMETRY_MATRIX4_H
 #define FLAT_GEOMETRY_MATRIX4_H
 
+#include <iomanip>
+#include <ostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -9,6 +11,30 @@ namespace flat
 {
 
 using Matrix4 = glm::mat4;
+
+inline std::ostream& operator<<(std::ostream& out, const Matrix4& m)
+{
+	const int w = 8;
+	const int p = 3;
+
+	std::streamsize precision = out.precision();
+
+	out.precision(p);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		out << "("
+			<< std::setw(w) << m[0][i] << " "
+			<< std::setw(w) << m[1][i] << " "
+			<< std::setw(w) << m[2][i] << " "
+			<< std::setw(w) << m[3][i]
+			<< ")" << std::endl;
+	}
+
+	out.precision(precision);
+
+	return out;
+}
 
 using glm::translate;
 using glm::rotate;
--- src/misc/vector.h
+++ src/misc/vector.h
@@ -1,6 +1,7 @@
 #ifndef FLAT_GEOMETRY_VECTOR2_H
 #define FLAT_GEOMETRY_VECTOR2_H
 
+#include <ostream>
 #include <glm/glm.hpp>
 #include <glm/gtx/norm.hpp>
 #include "../debug/assert.h"
@@ -16,6 +17,24 @@ inline float vector2_angle(const Vector2& vector2)
 {
 	FLAT_ASSERT(vector2.x != 0 || vector2.y != 0);
 	return std::atan2f(vector2.y, vector2.x);
+}
+
+inline std::ostream& operator<<(std::ostream& out, const Vector2& v)
+{
+	out << "Vector2(" << v.x << "," << v.y << ")";
+	return out;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const Vector3& v)
+{
+	out << "Vector3(" << v.x << "," << v.y << "," << v.z << ")";
+	return out;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const Vector4& v)
+{
+	out << "Vector4(" << v.x << "," << v.y << "," << v.z << "," << v.w << ")";
+	return out;
 }
 
 using glm::length;
