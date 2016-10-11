3a1c6075d592db28d7a753b2736b704da0b3c358
mcdooda <nicolas.dod@gmail.com>
2016-10-11 21:25:16 +0200

    Added StateImpl<T>, specialized version of State

--- src/state/state.h
+++ src/state/state.h
@@ -19,6 +19,37 @@ class State
 		virtual void exit(Agent& agent) = 0;
 };
 
+template <class T>
+class StateImpl : public State
+{
+	public:
+		void enter(Agent& agent) override final;
+		void execute(Agent& agent) override final;
+		void exit(Agent& agent) override final;
+
+		virtual void enter(T& agent) = 0;
+		virtual void execute(T& agent) = 0;
+		virtual void exit(T& agent) = 0;
+};
+
+template<class T>
+void StateImpl<T>::enter(Agent& agent)
+{
+	enter(agent.to<T>());
+}
+
+template<class T>
+void StateImpl<T>::execute(Agent& agent)
+{
+	execute(agent.to<T>());
+}
+
+template<class T>
+void StateImpl<T>::exit(Agent& agent)
+{
+	exit(agent.as<T>());
+}
+
 } // state
 } // flat
 
