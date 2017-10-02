cab4e1cd991bceda1dd055d10db88122fb129faf
mcdooda <nicolas.dod@gmail.com>
2017-10-02 22:35:30 +0200

    Fixed bool node

--- lua/graph/script/nodes/boolnode.lua
+++ lua/graph/script/nodes/boolnode.lua
@@ -12,12 +12,12 @@ function BoolNode:execute(runtime)
 end
 
 function BoolNode:setValue(value)
-    assert(flat.type(value) == flat.types.BOOLEAN, 'Wrong type for setValue: boolean expected, got ' .. flat.typename(value))
+    assert(flat.type(value) == flat.types.BOOLEAN, 'Wrong type for setValue: boolean expected, got ' .. flat.typetostring(flat.type(value)))
     self.value = value
 end
 
 function BoolNode:getValue()
-    assert(flat.type(self.value) == flat.types.BOOLEAN, 'Wrong type for self.value: boolean expected, got ' .. flat.typename(self.value))
+    assert(flat.type(self.value) == flat.types.BOOLEAN, 'Wrong type for self.value: boolean expected, got ' .. flat.typetostring(flat.type(value)))
     return self.value
 end
 
