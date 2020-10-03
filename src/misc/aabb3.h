6356ab58c6b0a539a01845a1665d7d3c7d622b58
mcdooda <nicolas.dod@gmail.com>
2020-10-03 12:41:50 +0100

    Added AABB3::overlapInclusive()

--- src/misc/aabb3.h
+++ src/misc/aabb3.h
@@ -55,6 +55,16 @@ class AABB3
 				&& a.min.z < b.max.z;
 		}
 
+		inline static bool overlapInclusive(const AABB3& a, const AABB3& b)
+		{
+			return a.max.x >= b.min.x
+				&& a.min.x <= b.max.x
+				&& a.max.y >= b.min.y
+				&& a.min.y <= b.max.y
+				&& a.max.z >= b.min.z
+				&& a.min.z <= b.max.z;
+		}
+
 	public:
 		Vector3 min;
 		Vector3 max;
