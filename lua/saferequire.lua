6bfe65168f5cc8b661ea6c765d1f2cd5f09a8cff
mcdooda <nicolas.dod@gmail.com>
2020-11-09 20:45:25 +0000

    Improved error reporting inside flat.safeRequire

--- lua/saferequire.lua
+++ lua/saferequire.lua
@@ -1,10 +1,16 @@
 local function safeRequire(require, file)
-    local results = { pcall(require, file) }
+    local callstack
+    local function errorHandler(...)
+        callstack = debug.traceback()
+        return ...
+    end
+    local results = { xpcall(require, errorHandler, file) }
     if not results[1] then
         local message = results[2]
         if message:match('module \'.+\' not found') then
             return nil
         else
+            message = message .. '\n' .. callstack
             error(message)
         end
     end
