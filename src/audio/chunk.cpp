11cb0826d86f6782964f34e43a5c0077598dced6
mcdooda <nicolas.dod@gmail.com>
2020-11-20 23:22:56 +0000

    Added error logging when Chunk::playChannel fails

--- src/audio/chunk.cpp
+++ src/audio/chunk.cpp
@@ -9,7 +9,8 @@ Chunk::Chunk(const std::string& filename)
 {
     m_chunk = Mix_LoadWAV(filename.c_str());
 //TODO error checking
-    if(!m_chunk) {
+    if (!m_chunk)
+    {
         std::cout <<"Mix_LoadWAV error: " << Mix_GetError() << std::endl;
     }
     FLAT_ASSERT(m_chunk != nullptr);
@@ -22,7 +23,12 @@ Chunk::~Chunk()
 
 int Chunk::playChannel(int loops) const
 {
-    return Mix_PlayChannel(-1, m_chunk, loops);
+    int channel = Mix_PlayChannel(-1, m_chunk, loops);
+    if (channel == -1)
+    {
+        std::cout << "Mix_PlayChannel error: " << Mix_GetError() << std::endl;
+    }
+    return channel;
 }
 
 }
