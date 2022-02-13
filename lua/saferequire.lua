db75be38fbb6d9c56e5704b2efb29a3ab4810791
mcdooda <nicolas.dod@gmail.com>
2022-02-13 19:12:15 +0000

    Added comment for bad performance in flat.safeRequire

--- lua/saferequire.lua
+++ lua/saferequire.lua
@@ -7,6 +7,7 @@ local function safeRequire(require, file)
     local results = { xpcall(require, errorHandler, file) }
     if not results[1] then
         local message = results[2]
+        -- TODO: fix that, this is bad for performance and inaccurate (the module could be found but require another module that cannot be found)
         if message:match('module \'.+\' not found') then
             return nil
         else
